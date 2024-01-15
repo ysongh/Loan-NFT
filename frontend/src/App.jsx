e5734d0df805d5922379f7c370a5dd758a4768a1
You Song Hou <ysongweb3@gmail.com>
2024-01-15 01:11:47 -0500

    feat: change the chain

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import { ChakraProvider, Text } from '@chakra-ui/react';
 import { WagmiConfig, createConfig } from "wagmi";
 import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
+import { sepolia } from "wagmi/chains";
 
 import { ALCHEMY_ID, WALLETCONNECT_PROJECT_ID } from "./keys";
+
+const chains = [sepolia];
 
 const config = createConfig(
   getDefaultConfig({
@@ -17,6 +20,8 @@ const config = createConfig(
     appDescription: "Your App Description",
     appUrl: "https://family.co", // your app's url
     appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+
+    chains,
   }),
 );
 
