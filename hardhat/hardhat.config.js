bb7024f93761f16957d943221bc04c9308a1e516
You Song Hou <ysongweb3@gmail.com>
2024-01-18 22:48:59 -0500

    feat: update hardhat.config.js to deploy a contract on Sepolia Testnet

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,6 +4,15 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    // npx hardhat run scripts/deploy.js --network sepolia
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+      accounts: [process.env.PRIVATEKEY],
+      chainId: 11155111,
+      gasPrice: 25000000000
+    },
+  },
   paths: {
     artifacts: '../frontend/src/artifacts',
     cache: '../frontend/src/cache',
