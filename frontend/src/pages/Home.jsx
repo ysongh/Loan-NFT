f324166473d841438921c89024a68187917dc340
You Song Hou <ysongweb3@gmail.com>
2024-01-20 15:34:43 -0500

    feat: add approve for Gho Token

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import { useContractWrite } from 'wagmi';
+import { Button } from '@chakra-ui/react';
 
 import VaultABI from "../artifacts/contracts/Vault.sol/Vault.json";
+import GHOABI from "../GHO.json";
 
 function Home() {
-  const { write } = useContractWrite({
+  const { write: approve } = useContractWrite({
+    address: "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60",
+    abi: GHOABI.abi,
+    functionName: 'approve',
+    args: ["0xc0B022D8c28fDE375Bf4D032cdD1b94ae507D383", "1000000000000000000"],
+    onError(error) {
+      console.log(error);
+    },
+  });
+
+  const { write: deposit } = useContractWrite({
     address: "0xc0B022D8c28fDE375Bf4D032cdD1b94ae507D383",
     abi: VaultABI.abi,
     functionName: 'deposit',
-    args: ["0"],
+    args: ["1000000000000000000"],
     onError(error) {
       console.log(error);
     },
@@ -16,7 +28,13 @@ function Home() {
   return (
     <>
       <h1>Test</h1>
-      <button disabled={!write} onClick={() => write?.()}>Deposit</button>
+      <Button disabled={!approve} onClick={() => approve?.()} mb="2">
+        Approve
+      </Button>
+      <br />
+      <Button disabled={!deposit} onClick={() => deposit?.()}>
+        Deposit
+      </Button>
     </>
   );
 };
