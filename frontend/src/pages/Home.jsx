3e0e4d6cef654814e69eba05a591270d2d0cdf32
You Song Hou <ysongweb3@gmail.com>
2024-01-20 20:02:25 -0500

    feat: display the balance for GHO

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useContractRead, useContractWrite, useAccount } from 'wagmi';
+import { formatEther } from 'viem'
 import { Container, Button } from '@chakra-ui/react';
 
 import VaultABI from "../artifacts/contracts/Vault.sol/Vault.json";
@@ -10,6 +11,17 @@ function Home() {
   const { data: balance } = useContractRead({
     address: "0xc0B022D8c28fDE375Bf4D032cdD1b94ae507D383",
     abi: VaultABI.abi,
+    functionName: 'balanceOf',
+    args: [address],
+    onError(error) {
+      console.log(error);
+    },
+  });
+
+
+  const { data: GHOBalance } = useContractRead({
+    address: "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60",
+    abi: GHOABI.abi,
     functionName: 'balanceOf',
     args: [address],
     onError(error) {
@@ -50,7 +62,8 @@ function Home() {
   return (
     <Container>
       <h1>Test</h1>
-      <p>{balance?.toString()}</p>
+      <p>{formatEther(GHOBalance)} GHO</p>
+      <p>{formatEther(balance)} Deposit</p>
       <Button disabled={!approve} onClick={() => approve?.()} mb="2">
         Approve
       </Button>
